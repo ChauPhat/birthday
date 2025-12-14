036a16cb792270b1e8a5ca628fb5c6d024cb1e72
chauphat <hieuphung2111@gmail.com>
2025-12-14 10:42:27 +0700

    test

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://birthday-rose-delta.vercel.app', // Thay bằng URL Vercel của bạn
+  methods: ['GET', 'POST']
+}));
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, '../public')));
@@ -43,5 +46,5 @@ app.get('*', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
